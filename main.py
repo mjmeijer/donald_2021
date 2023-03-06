83ed71c333c97989dbbfd559b106450b7f266b37
Maarten Meijer <mjmeijer@fold1.com>
2023-03-06 13:31:02 +0100

    dependabot warnings

--- main.py
+++ main.py
@@ -170,5 +170,5 @@ if __name__ == '__main__':
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     # app.run(host='127.0.0.1', port=8080, debug=True)
-    app.run(debug=True)
+    app.run(debug=False)
 # [END gae_python38_app]
