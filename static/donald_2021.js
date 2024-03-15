64f84c36bd3b29245a6efc8abbdcc9d8bc0e4d89
Maarten Meijer <mjmeijer@eclipsophy.com>
2024-03-15 11:04:31 +0100

    timeout behavior return new state

--- static/donald_2021.js
+++ static/donald_2021.js
@@ -302,9 +302,13 @@ function handleTimeOut() {
   showTimeout();
   // TODO post the result as timeout
   if (frameCount - startFrame > T5_TIMEOUT) {
-    testLevel = 0;
+    if(typeof custom_timeout === "function"){
+      change_state(custom_timeout());
+    } else {
+      testLevel = 0;
+      changeState(0);
+    }
 //    print("Timeout!");
-    changeState(0);
   }
 }
 
